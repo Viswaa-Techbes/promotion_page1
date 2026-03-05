8ceb69dfe86905cad8b6e57959f01a8d8d0f0643
ViswaaE <viswaaeswaran@gmail.com>
2026-03-05 21:52:54 +0530

    login button push

--- src/app/page.js
+++ src/app/page.js
@@ -3,6 +3,7 @@ import React, { useState, useEffect } from 'react';
 import Image from 'next/image';
 import { useRouter } from "next/navigation";
 import LoginModal from "@/components/LoginModal";
+import { useSearchParams } from "next/navigation";
 
 const PromoSection = () => {
   const router = useRouter();
